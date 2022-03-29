ef3abb7b9dde1a96aee304169de70272a937cad7
Aaron <aaron@merchandise-essentials.com>
2022-03-29 17:27:10 +0200

    trigger script

--- Technical_Files/trigger_script.js
+++ Technical_Files/trigger_script.js
@@ -4,6 +4,6 @@
 // <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    // $('head').append('<link rel="stylesheet" type="text/css" href="Technical_Files/new_style.css">');
+    // cd $('head').append('<link rel="stylesheet" type="text/css" href="Technical_Files/new_style.css">');
     console.log("dab dab die dooeb")
 });
