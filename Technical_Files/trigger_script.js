da808c20a68bd512966321b964902bfc7fe82ec6
Aaron <aaron@merchandise-essentials.com>
2022-03-29 17:25:23 +0200

    added triggrr script

---
+++ Technical_Files/trigger_script.js
@@ -0,0 +1,9 @@
+// Call stylesheet
+// Call style script
+
+// <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    // $('head').append('<link rel="stylesheet" type="text/css" href="Technical_Files/new_style.css">');
+    console.log("dab dab die dooeb")
+});
