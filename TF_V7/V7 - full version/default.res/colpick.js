68fcd7f451c82516275d3f3a9219d322194be2ed
Aaron <aaron@merchandise-essentials.com>
2022-03-31 10:10:53 +0200

    finished script to add first image to tables as a background

--- TF_V7/V7 - full version/default.res/colpick.js
+++ TF_V7/V7 - full version/default.res/colpick.js
@@ -1,3 +1,58 @@
+// EDITS BY AARON
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Change logo to white Sunday logo
+    let sundayLogo = document.querySelectorAll(".logo-img");
+
+    for (let i = 0; i < sundayLogo.length; i++) {
+        sundayLogo[i].src="https://teamsunday.com/wp-content/uploads/2022/03/Sunday_Full_White.png";
+    }
+
+    // Center Sunday logo on a black background
+    let headerTF = document.querySelectorAll(".header-table > tbody > tr:first-child > td:nth-child(3)");
+
+    for (let i = 0; i < headerTF.length; i++) {
+        headerTF[i].setAttribute("colspan", "8");
+        headerTF[i].classList.add("center");
+    }
+
+    // Delete matching style column
+    let matchingStyleCol = document.querySelectorAll(".header-table > tbody > tr:nth-child(2) > td:nth-child(3)");
+
+    for (let i = 0; i < matchingStyleCol.length; i++) {
+        matchingStyleCol[i].classList.add("hidden");
+    }
+
+    // Change "hoodie" to "color" in table head
+    let replaceHoodie = document.querySelectorAll(".table-standard.bom-page-table.bom_table > thead > tr > th:nth-child(6)");
+
+    for (let i = 0; i < replaceHoodie.length; i++) {
+        replaceHoodie[i].innerHTML = "Color";
+    }
+
+    // Select top mock up
+    let firstIMG = document.querySelector(".image-container > img");
+    console.log(firstIMG);
+    console.log(firstIMG.getAttribute("src"));
+
+    // Select IMG destination
+    let IMGPlaceholder = document.querySelectorAll(".table-standard.bom-page-table.bom_table");
+    console.log(IMGPlaceholder);
+
+    for (let i = 0; i < IMGPlaceholder.length; i++) {
+        console.log(IMGPlaceholder);
+        IMGPlaceholder[i].classList.add("set-background");
+    }
+});
+
+
+
+
+
+
+
+// END EDITS BY AARON
+
+
 /*
 colpick Color Picker
 Copyright 2013 Jose Vargas. Licensed under GPL license. Based on Stefan Petre's Color Picker www.eyecon.ro, dual licensed under the MIT and GPL licenses
