9922fcce19d9700ef8db7b07312e0fd386baf18c
Aaron <gerardaaron1997@gmail.com>
2022-05-04 14:51:49 +0200

    progress on the menu

--- teamsunday.com/HTML/header/header.js
+++ teamsunday.com/HTML/header/header.js
@@ -1,14 +1,31 @@
 $(document).ready(function() {
-    // Handler for .ready() called.
+
+    // Set active state on clicked element
+        // remove show from all other but clicked elments' child
+        // add show to clicked element child
 
 
 
 
 
+    // Clicked outside of dropdown => close menu
 
 
 
+    // Define button
+    // $('#submit').on('click', function () {
+    //     // the element that was clicked:
+    //     $(this)
+    //         // finding the first ancestor <header> element:
+    //         .closest('header')
+    //         // finding the next sibling, if it matches the selector:
+    //         .next('.toggleclass')
+    //        // adding the 'open' class-name:
+    //        .addClass('open');
+    // });
 
+
+    // Small menu script
 
 
 
