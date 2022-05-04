2154e06d8294606225d98d455ca25a79aeb03531
Aaron <gerardaaron1997@gmail.com>
2022-05-04 16:59:58 +0200

    progress on mega menu

--- teamsunday.com/HTML/header/header.js
+++ teamsunday.com/HTML/header/header.js
@@ -1,32 +1,13 @@
 $(document).ready(function() {
-
-    // Set active state on clicked element
-        // remove show from all other but clicked elments' child
-        // add show to clicked element child
-
-
+    // Show dropdown on click
+    $(".nav-toggle").on('click', function() {
+        $(this).next().toggleClass("show").siblings().removeClass("show");
+    });
 
 
 
-    // Clicked outside of dropdown => close menu
-
-
-
-    // Define button
-    // $('#submit').on('click', function () {
-    //     // the element that was clicked:
-    //     $(this)
-    //         // finding the first ancestor <header> element:
-    //         .closest('header')
-    //         // finding the next sibling, if it matches the selector:
-    //         .next('.toggleclass')
-    //        // adding the 'open' class-name:
-    //        .addClass('open');
-    // });
-
-
-    // Small menu script
-
+    // Add focus class to "open" nav item
+    
 
 
 
