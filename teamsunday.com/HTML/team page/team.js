f15000528fd552a0195ad5800f3ab451c0ada494
Aaron Gerard <gerardaaron1997@gmail.com>
2022-06-30 15:57:13 +0200

    copied all exisiting employees to employees array

--- teamsunday.com/HTML/team page/team.js
+++ teamsunday.com/HTML/team page/team.js
@@ -152,6 +152,193 @@ const team = [
         'USP_3' : 'Considers winning ‘best halloween costume’ as one of her greatest professional achievements'
     },
     {
+        'name' : 'Nicola',
+        'role' : 'Account Executive',
+        'country' : 'Belgium',
+        'img' : '/wp-content/uploads/2021/11/NicolaNeve.jpg',
+        'USP_1' : 'Salesman who loves a good laugh',
+        'USP_2' : 'Loves cycling or playing padel during the weekend',
+        'USP_3' : 'Nicola’s last name is ‘Neve’, which is Portuguese for ‘snow’. He learned this during a meeting with a client'
+    },
+    {
+        'name' : 'Timon',
+        'role' : 'Account Executive',
+        'country' : 'Belgium',
+        'img' : '/wp-content/uploads/2021/11/TimonDedeyster.jpg',
+        'USP_1' : 'Salesman specialised in HR, business consultancy and Telecom clients',
+        'USP_2' : 'Loves watching ‘Casa De Papel’ on Netflix',
+        'USP_3' : 'He’s the quiet type, but will suddenly pull a 450K deal out of nowhere'
+    },
+    {
+        'name' : 'Basiel',
+        'role' : 'Account Manager',
+        'country' : 'Belgium',
+        'img' : '/wp-content/uploads/2021/11/BasielSinnesael.jpg',
+        'USP_1' : 'Salesman with keen interest in cities and food & drink',
+        'USP_2' : 'Loves padel, barbecue and beers',
+        'USP_3' : 'Owner of a great Spotify playlist called ‘zet ma tandje luwre’. It’s a banger!'
+    },
+    {
+        'name' : 'Anna',
+        'role' : 'SDR Coordinator',
+        'country' : 'Belgium',
+        'img' : '/wp-content/uploads/2021/11/AnnaBaffour.jpeg',
+        'USP_1' : 'Loves meeting friends and binge watching Netflix on Sundays',
+        'USP_2' : 'Thinks brunch is the best invention ever because it means sleeping in and waking up to Mimosas'
+    },
+    {
+        'name' : 'Charlotte',
+        'role' : 'Sales Development Representative',
+        'country' : 'Belgium',
+        'img' : '/wp-content/uploads/2021/11/CharlotteVens.jpeg',
+        'USP_1' : 'Loves to surf and go on adventures on Sundays',
+        'USP_2' : 'If you’re looking for Charlotte during the weekend, there’s a big chance you will find her touring the Netherlands or France in her van'
+    },
+    {
+        'name' : 'Ana',
+        'role' : 'Sales Development Representative',
+        'country' : 'Belgium',
+        'img' : '',
+        'USP_1' : '',
+        'USP_2' : '',
+        'USP_3' : ''
+    },
+    {
+        'name' : 'Joeri',
+        'role' : 'Sales Development Representative',
+        'country' : 'Netherlands',
+        'img' : '',
+        'USP_1' : '',
+        'USP_2' : '',
+        'USP_3' : ''
+    },
+    {
+        'name' : 'Sarah',
+        'role' : 'Sales Development Representative',
+        'country' : 'Ireland',
+        'img' : '',
+        'USP_1' : '',
+        'USP_2' : '',
+        'USP_3' : ''
+    },
+    {
+        'name' : 'Chiara',
+        'role' : 'Receptionist',
+        'country' : 'Belgium',
+        'img' : '/wp-content/uploads/2021/11/ChiaraDetavernier.jpg',
+        'USP_1' : 'Office MVP',
+        'USP_2' : 'On Sundays, she loves to go out for dinner and watch Netflix',
+        'USP_3' : 'Riding a cow is high on her bucket list'
+    },
+    {
+        'name' : 'Midas',
+        'role' : 'Country Executive',
+        'country' : 'Netherlands',
+        'img' : '/wp-content/uploads/2021/11/MidasStreekstra.jpeg',
+        'USP_1' : 'Outgoing and social, or having a drink in the city with friends',
+        'USP_2' : 'On Sundays, he supports and talks about Ajax',
+        'USP_3' : 'Better not to tell him that Diemen is not part of Amsterdam because that will open a long discussion'
+    },
+    {
+        'name' : 'Eva',
+        'role' : 'Account Manager',
+        'country' : 'Netherlands',
+        'img' : '/wp-content/uploads/2021/11/EvaDeveer.jpg',
+        'USP_1' : 'True summer person',
+        'USP_2' : 'On Sunday she enjoys winning a field hockey game',
+        'USP_3' : 'Loves to enjoy good weather from her boat in Amsterdam'
+    },
+    {
+        'name' : 'Max',
+        'role' : 'Account Executive',
+        'country' : 'Netherlands',
+        'img' : '/wp-content/uploads/2022/03/MaxPierik.png',
+        'USP_1' : 'On Sundays, you can probably find him in the soccer stadium to watch Liverpool',
+        'USP_2' : 'Likes to practice as many sports as possible',
+        'USP_3' : 'His favorite pastime is to have drinks and good food with his friends'
+    },
+    {
+        'name' : 'Carlin',
+        'role' : 'Account Executive',
+        'country' : 'Netherlands',
+        'img' : '',
+        'USP_1' : '',
+        'USP_2' : '',
+        'USP_3' : ''
+    },
+    {
+        'name' : 'Sebastiaan',
+        'role' : 'International Sales Manager',
+        'country' : 'Belgium',
+        'img' : '/wp-content/uploads/2021/11/SebastiaanMaesen.jpg',
+        'USP_1' : 'Is an extraverted introvert',
+        'USP_2' : 'On sunday he Netflixes, practises some Sports, goes out for brunch',
+        'USP_3' : 'Fav Quote: “Some birds are too colorful to be caged”'
+    },
+    {
+        'name' : 'Alexis',
+        'role' : '',
+        'country' : 'Germany',
+        'img' : '/wp-content/uploads/2021/11/AlexisKivu.jpg',
+        'USP_1' : 'Open minded and curious',
+        'USP_2' : 'Loves to go to the gym and spend time in nature on Sunday',
+        'USP_3' : 'Alexis welcomed Angela Merkel in his hometown and gave her flowers wearing a bright red coat when he was a kid'
+    },
+    {
+        'name' : 'Laura',
+        'role' : '',
+        'country' : 'Germany',
+        'img' : '/wp-content/uploads/2022/03/LauraKraft.png',
+        'USP_1' : 'Loves Pop Music and Trash TV',
+        'USP_2' : 'Would spend every penny on traveling or her super cute Dog Alfie',
+        'USP_3' : 'Wins people over with her brutal honesty and silly jokes'
+    },
+    {
+        'name' : 'Louis',
+        'role' : 'Head Of Demand Generation',
+        'country' : 'Belgium',
+        'img' : '/wp-content/uploads/2021/11/LouisVandommele.jpg',
+        'USP_1' : 'Very sociable and musically talented, ex professional drummer',
+        'USP_2' : 'Loves sneakers and vintage clothing',
+        'USP_3' : 'Is often quoted saying "Europe is a village"'
+    },
+    {
+        'name' : 'Ankur',
+        'role' : '',
+        'country' : 'Ireland',
+        'img' : '/wp-content/uploads/2021/11/AnkurBaliyan.jpg',
+        'USP_1' : 'Cricket freak',
+        'USP_2' : 'On Sunday, he loves cooking Indian food recipes and secretly watching his childhood cartoons',
+        'USP_3' : 'The hardest thing for him is to hide what he wants to say'
+    },
+    {
+        'name' : 'Sohair',
+        'role' : '',
+        'country' : 'Ireland',
+        'img' : '',
+        'USP_1' : '',
+        'USP_2' : '',
+        'USP_3' : ''
+    },
+    {
+        'name' : '',
+        'role' : '',
+        'country' : '',
+        'img' : '',
+        'USP_1' : '',
+        'USP_2' : '',
+        'USP_3' : ''
+    },
+    {
+        'name' : '',
+        'role' : '',
+        'country' : '',
+        'img' : '',
+        'USP_1' : '',
+        'USP_2' : '',
+        'USP_3' : ''
+    },
+    {
         'name' : '',
         'role' : '',
         'country' : '',
