9c800c3a3af2e6d210dea70561bd96e1d3813ed2
Aaron <gerardaaron1997@gmail.com>
2022-07-06 16:48:23 +0200

    commented base code for future reference

--- WooCommerce/add_currency.php
+++ WooCommerce/add_currency.php
@@ -12,9 +12,7 @@ function add_tsx_currency_symbol( $custom_currency_symbol, $custom_currency ) {
 }
 
 
-BASE
-
-add_filter( 'woocommerce_currencies', 'add_cw_currency' );
+<!-- add_filter( 'woocommerce_currencies', 'add_cw_currency' );
 function add_cw_currency( $cw_currency ) {
      $cw_currency['CLOUDWAYS'] = __( 'CLOUDWAYS CURRECY', 'woocommerce' );
      return $cw_currency;
@@ -25,4 +23,4 @@ function add_cw_currency_symbol( $custom_currency_symbol, $custom_currency ) {
          case 'CLOUDWAYS': $custom_currency_symbol = 'CW$'; break;
      }
      return $custom_currency_symbol;
-}
+} -->
