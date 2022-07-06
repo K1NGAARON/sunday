8a99ce76855ac2e4bf710a34aa3785b5410d639e
Aaron <gerardaaron1997@gmail.com>
2022-07-06 14:58:51 +0200

    created TSX coin

--- WooCommerce/add_currency.php
+++ WooCommerce/add_currency.php
@@ -1,3 +1,19 @@
+add_filter( 'woocommerce_currencies', 'add_tsx_currency' );
+function add_cw_currency( $cw_currency ) {
+     $tsx_currency['SUNDAY'] = __( 'SUNDAY CURRECY', 'woocommerce' );
+     return $tsx_currency;
+}
+add_filter('woocommerce_currency_symbol', 'add_tsx_currency_symbol', 10, 2);
+function add_tsx_currency_symbol( $custom_currency_symbol, $custom_currency ) {
+     switch( $custom_currency ) {
+         case 'SUNDAY': $custom_currency_symbol = 'TSX$'; break;
+     }
+     return $custom_currency_symbol;
+}
+
+
+BASE
+
 add_filter( 'woocommerce_currencies', 'add_cw_currency' );
 function add_cw_currency( $cw_currency ) {
      $cw_currency['CLOUDWAYS'] = __( 'CLOUDWAYS CURRECY', 'woocommerce' );
