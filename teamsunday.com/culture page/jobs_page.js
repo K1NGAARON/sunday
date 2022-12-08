0c8f2bc647cdf51d7e9e500e4c03cf3036659312
Aaron Gerard <gerardaaron1997@gmail.com>
2022-12-08 13:47:04 +0100

    changes

--- teamsunday.com/culture page/jobs_page.js
+++ teamsunday.com/culture page/jobs_page.js
@@ -18,137 +18,162 @@ function dropdownFilter() {
     });
 };
 
+const jobsIMG = [
+    'https://teamsunday.com/wp-content/uploads/2022/12/image-1.jpg',
+    'https://teamsunday.com/wp-content/uploads/2022/12/image-2.jpg',
+    'https://teamsunday.com/wp-content/uploads/2022/12/image-3.jpg',
+    'https://teamsunday.com/wp-content/uploads/2022/12/image-4.jpg',
+    'https://teamsunday.com/wp-content/uploads/2022/12/image-5.jpg',
+    'https://teamsunday.com/wp-content/uploads/2022/12/image-6.jpg',
+    'https://teamsunday.com/wp-content/uploads/2022/12/image-7.jpg',
+    'https://teamsunday.com/wp-content/uploads/2022/12/image-8.jpg',
+    'https://teamsunday.com/wp-content/uploads/2022/12/image-9.jpg',
+    'https://teamsunday.com/wp-content/uploads/2022/12/image-10.jpg'
+];
+
 const jobsWrapper = document.querySelector("#jobs-section");
 const jobsArray = [
     {
-        'positionTitle' : 'Stage HR',
+        'positionTitle' : 'HR Traineeship',
         'positionText' : 'Als trainee van team Sunday bouw je dagelijks mee aan dit ambassadeurschap.',
-        'positionIMG' : 'https://teamsunday.com/wp-content/uploads/2022/05/DE.jpg',
-        'positionALT' : 'stage HR',
+        'positionALT' : 'HR Traineeship',
         'positionURL' : '/en/jobs/stage-hr',
         'positionCountry' : 'Belgium',
         'positionCity' : 'Roeselare',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png' 
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png',
+        'order' : '9'
     },
     {
         'positionTitle' : 'Sales Traineeship Amsterdam',
         'positionText' : 'Ben je ambitieus en leg je graag contacten? Ben je op zoek naar een uitdaging in een internationaal bedrijf?',
-        'positionIMG' : 'https://teamsunday.com/wp-content/uploads/2022/05/BE2.jpg',
         'positionALT' : 'Sales Traineeship at Sunday',
         'positionURL' : '/en/jobs/sales-traineeship/',
         'positionCountry' : 'Netherlands',
         'positionCity' : 'Amsterdam',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Netherlands-Flag.png' 
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Netherlands-Flag.png',
+        'order' : '11'
     },
     {
         'positionTitle' : 'Sales Traineeship Roeselare',
         'positionText' : 'Ben je ambitieus en leg je graag contacten? Ben je op zoek naar een uitdaging in een internationaal bedrijf?',
-        'positionIMG' : 'https://teamsunday.com/wp-content/uploads/2022/05/EXTRA3.jpg',
         'positionALT' : 'Sales Traineeship at Sunday',
         'positionURL' : '/en/jobs/sales-traineeship/',
         'positionCountry' : 'Belgium',
         'positionCity' : 'Roeselare',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png' 
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png',
+        'order' : '11'
     },
-    {
-        'positionTitle' : 'Financial Controller',
-        'positionText' : 'Je bent verantwoordelijk om de financiële rapportering van de groep tot een hoger niveau te tillen.',
-        'positionIMG' : '/wp-content/uploads/2022/05/DE.jpg',
-        'positionALT' : 'Financial controller at Sunday',
-        'positionURL' : '/en/jobs/financial-controller-roeselare/',
-        'positionCountry' : 'Belgium',
-        'positionCity' : 'Roeselare',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png' 
-    },
+    // {
+    //     'positionTitle' : 'Financial Controller',
+    //     'positionText' : 'Je bent verantwoordelijk om de financiële rapportering van de groep tot een hoger niveau te tillen.',
+    //     'positionALT' : 'Financial controller at Sunday',
+    //     'positionURL' : '/en/jobs/financial-controller-roeselare/',
+    //     'positionCountry' : 'Belgium',
+    //     'positionCity' : 'Roeselare',
+    //     'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png',
+    //     'order' : ''
+    // },
     {
         'positionTitle' : 'Account Executive',
         'positionText' : 'Our business grows with you. Close some interesting deals with us!',
-        'positionIMG' : 'https://teamsunday.com/wp-content/uploads/2022/05/DE.jpg',
         'positionALT' : 'Account executive at Sunday',
         'positionURL' : '/en/jobs/account-executive-germany/',
         'positionCountry' : 'Germany',
         'positionCity' : 'Berlin',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Germany-Flag.png'
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Germany-Flag.png',
+        'order' : '3'
     },
     {
         'positionTitle' : 'Account Executive',
         'positionText' : 'Our business grows with you. Close some interesting deals with us!',
-        'positionIMG' : 'https://teamsunday.com/wp-content/uploads/2022/05/EXTRA3.jpg',
         'positionALT' : 'Account executive at Sunday',
         'positionURL' : '/en/jobs/account-executive-belgium/',
         'positionCountry' : 'Belgium',
         'positionCity' : 'Roeselare',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png'
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png',
+        'order' : '1'
+    },
+    {
+        'positionTitle' : 'Payroll & fleet officer',
+        'positionText' : 'Als payroll & fleet officer zal je ons verrijken met je reeds opgebouwde kennis & ons energiek HR team versterken.',
+        'positionALT' : 'Payroll & fleet officer',
+        'positionURL' : '/en/jobs/payroll-fleet-officer/',
+        'positionCountry' : 'Belgium',
+        'positionCity' : 'Roeselare',
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png',
+        'order' : '1'
     },
     {
         'positionTitle' : 'Account Executive',
         'positionText' : 'Our business grows with you. Close some interesting deals with us!',
-        'positionIMG' : 'https://teamsunday.com/wp-content/uploads/2022/05/IRL.jpg',
         'positionALT' : 'Account executive at Sunday',
         'positionURL' : '/en/jobs/account-executive-ireland/',
         'positionCountry' : 'Ireland',
         'positionCity' : 'Dublin',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Ireland-Flag.png'
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Ireland-Flag.png',
+        'order' : '7'
     },
     {
         'positionTitle' : 'Account Executive',
         'positionText' : 'Our business grows with you. Close some interesting deals with us!',
-        'positionIMG' : '/wp-content/uploads/2022/05/DE.jpg',
         'positionALT' : 'Account executive at Sunday',
         'positionURL' : '/en/jobs/account-executive-netherlands/',
         'positionCountry' : 'Netherlands',
         'positionCity' : 'Amsterdam',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Netherlands-Flag.png'
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Netherlands-Flag.png',
+        'order' : '2'
     },
     {
         'positionTitle' : 'Sales Development Representative',
         'positionText' : 'Our business starts with you. Hunt for promising leads with us!',
-        'positionIMG' : 'https://teamsunday.com/wp-content/uploads/2022/05/NL.jpg',
         'positionALT' : 'Sales development representative at Sunday',
         'positionURL' : '/en/jobs/sales-development-representative-netherlands/',
         'positionCountry' : 'Netherlands',
         'positionCity' : 'Amsterdam',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Netherlands-Flag.png'
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Netherlands-Flag.png',
+        'order' : '5'
     },
     {
         'positionTitle' : 'Sales Development Representative',
         'positionText' : 'Our business starts with you. Hunt for promising leads with us!',
-        'positionIMG' : 'https://teamsunday.com/wp-content/uploads/2022/05/BE2.jpg',
         'positionALT' : 'Sales development representative at Sunday',
         'positionURL' : '/en/jobs/sales-development-representative-belgium/',
         'positionCountry' : 'Belgium',
         'positionCity' : 'Roeselare',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png'
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Belgium-Flag.png',
+        'order' : '4'
     },
     {
         'positionTitle' : 'Sales Development Representative',
         'positionText' : 'Our business starts with you. Hunt for promising leads with us!',
-        'positionIMG' : 'https://teamsunday.com/wp-content/uploads/2022/05/DE2.jpg',
         'positionALT' : 'Sales development representative at Sunday',
         'positionURL' : '/en/jobs/sales-development-representative-germany/',
         'positionCountry' : 'Germany',
         'positionCity' : 'Berlin',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Germany-Flag.png'
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Germany-Flag.png',
+        'order' : '6'
     },
     {
         'positionTitle' : 'Sales Development Representative',
         'positionText' : 'Our business starts with you. Hunt for promising leads with us!',
-        'positionIMG' : 'https://teamsunday.com/wp-content/uploads/2022/05/IRL2.jpg',
         'positionALT' : 'Sales development representative at Sunday',
         'positionURL' : '/en/jobs/sales-development-representative-ireland/',
         'positionCountry' : 'Ireland',
         'positionCity' : 'Dublin',
-        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Ireland-Flag.png'
+        'flagURL' : 'https://teamsunday.com/wp-content/uploads/2021/08/Ireland-Flag.png',
+        'order' : '8'
     }
 ];
 
 // Create job post wrapper
 function createJobBox() {
     for (let i = 0; i < jobsArray.length; i++) {
+        const randomImage = jobsIMG[Math.floor((Math.random() * jobsIMG.length))];
+
         const jobTemplate = `
-            <div class="job-post ${jobsArray[i].positionCountry}">
+            <div class="job-post ${jobsArray[i].positionCountry}" style="order: ${jobsArray[i].order}">
                 <div class="col">
-                    <img src="${jobsArray[i].positionIMG}" alt="${jobsArray[i].positionALT}">
+                    <img src="${randomImage}" alt="${jobsArray[i].positionALT}">
                 </div>
                 <div class="col">
                     <div class="country-wrapper">
@@ -171,8 +196,6 @@ function createJobBox() {
     };
 };
 
-$(document).ready(function() {
-    createJobBox();
-});
+$(document).ready(createJobBox);
 
 dropdown.addEventListener('change', dropdownFilter);
